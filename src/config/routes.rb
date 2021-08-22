968fc40300057c5a89092d6051246c8e30914d14
sho matsuda <qqq.ms1126@gmail.com>
2021-08-23 01:08:50 +0900

    scssの修正 #5

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#introduction'
   get 'static_pages/introduction'
+  root 'static_pages#introduction'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
