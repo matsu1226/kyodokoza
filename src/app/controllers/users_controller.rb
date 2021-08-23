6dd3e523cb6a63ef695ccf393fffc0c42d8fc341
sho matsuda <qqq.ms1126@gmail.com>
2021-08-23 22:36:19 +0900

    formの見た目完成,次はmailer使ったユーザー認証 #4

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      #
+    else
+      # flash[:error] = 
+      render "new"
+    end
   end
 
   def show
@@ -19,4 +27,10 @@ class UsersController < ApplicationController
 
   def index
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- src/spec/models/user_spec.rb
+++ src/spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, type: :model do
   before do
     @user = User.new(name: "正太郎", 
                     email: "example@gmail.com",
-                    password: "example",
-                    password_confirmation: "example")
+                    password: "example01",
+                    password_confirmation: "example01")
   end
   subject { @user }
   
@@ -14,9 +14,67 @@ RSpec.describe User, type: :model do
   it {should respond_to(:password)}
   it {should respond_to(:password_digest)}
 
-  describe "正しいuserを保存" do
+  describe "正しいuserを保存するテスト" do
     before { @user.save }
     it { should be_valid }
+  end
+
+  context "正しくないuserを保存するテスト" do
+    describe "名前が空欄" do
+      before { @user.name = " " }
+      it { should_not be_valid }
+    end
+
+    describe "名前が11文字以上" do
+      before { @user.name = "a" * 11 }
+      it { should_not be_valid }
+    end
+
+    describe "メールが空欄" do
+      before { @user.email = " " }
+      it { should_not be_valid }
+    end
+
+    describe "メールが重複" do
+      let(:user_with_same_email) { User.new(name: "健太", 
+                                            email: "example@gmail.com",
+                                            password: "example02",
+                                            password_confirmation: "example02")}
+      before do
+        @user.save
+        user_with_same_email.save
+      end
+      it { expect(user_with_same_email).not_to be_valid }
+    end
+
+    it "メールフォーマットがダメ" do
+      address = ["examplegmail.com", "@gmail.com", "example@g_mail.com", "example@gmailcom", "example@gmail.56"] 
+      address.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+    
+    describe "パスワードが空欄" do
+      before { @user.password = " " }
+      it { should_not be_valid }
+    end
+
+    describe "パスワードが7文字以下" do
+      before do
+        @user.password = "example" 
+        @user.password_confirmation = "example" 
+      end  
+      it { should_not be_valid }
+    end
+
+    describe "パスワードが20文字以上" do
+      before do
+        @user.password = "example01example01example01" 
+        @user.password_confirmation = "example01example01example01" 
+      end  
+      it { should_not be_valid }
+    end
   end
 
 end
