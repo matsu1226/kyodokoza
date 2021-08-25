dfa237a5c29f7c80e1c98514a8aeccbf1904264a
sho matsuda <qqq.ms1126@gmail.com>
2021-08-26 00:01:08 +0900

    gem for system specのインストール #35

---
+++ src/spec/system/users_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  pending "add some scenarios (or delete) #{__FILE__}"
+
+  
+/system/user_spec.rb
+
+context "ログイン前" do
+
+  入れない=>hedder&footer&user/show,edit, 
+
+  describe "新規登録" do
+    describe "メールアドレス重複エラー" do
+
+    end
+    
+    describe "正常な新規登録" do
+
+    end
+
+  end
+end
+
+context "ログイン後" do
+  入れる=>hedder&footer&user/show,edit, 
+
+  describe "ログアウト" do
+
+  end
+
+  describe "アカウント情報の編集" do
+    describe "名前の変更" do
+
+    end
+    
+    describe "パスワード変更リンク" do
+
+    end
+    
+    describe "アカウントの削除" do
+
+    end
+  end
+
+  describe "家族の登録" do
+
+    describe "家族の一覧" do
+
+    end
+  end
+
+  describe "エクセル出力" do
+
+  end
+
+end
+end
