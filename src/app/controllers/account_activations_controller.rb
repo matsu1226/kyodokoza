6c6fee1d879536917df73fe5326af1d37968469f
sho matsuda <qqq.ms1126@gmail.com>
2021-08-25 09:56:50 +0900

    users_showのレイアウト完成 #7

--- src/app/controllers/account_activations_controller.rb
+++ src/app/controllers/account_activations_controller.rb
@@ -4,7 +4,7 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
-      log_in user
+      # log_in user
       flash[:success] = "会員登録完了です！"
       # redirect_to new_post_path
       redirect_to "/introduction"
