80b581e26d510381b98f64f5c66bb49b3595d46b
sho matsuda <qqq.ms1126@gmail.com>
2021-09-20 21:59:06 +0900

    post indexのnarrow_downの修正

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -31,32 +31,34 @@ class PostsController < ApplicationController
 
 
   def narrow_down
-    make_sum_target_price(@relationship.categories) # @sum_target_priceの生成
-    # (1)月の変更 or (2)ユーザー絞り込み
-    # (1)例:現在9月の画面
-    # [view] posts_narrow_down_path(month: @month.prev_month) 
-    # [controller] params[:month] = @変更前month.prev_month = 2021/08
-    # [controller] @変更後month = @変更前month.prev_month = 2021/08
-    # (2)例:現在9月の画面
-    # [view] posts_narrow_down_path(month: @month)
-    # [controller] params[:month] = @変更前month = 2021/09
-    # [controller] @変更後month = @変更前month = 2021/09
-
     @month = Time.parse(params[:month]) 
-
+    
     family_user_ids = @relationship.user_ids
     family_category_ids = @relationship.category_ids
-
+    
     if params[:user_id].blank? && params[:category_id].blank?
+      @sum_target_price = Category.where(id: family_category_ids).sum(:target_price)
       @posts = narrow_downing_posts(family_user_ids, family_category_ids, @month)
     elsif params[:user_id].present?
+      @sum_target_price = Category.where(id: family_category_ids).sum(:target_price)
       @posts = narrow_downing_posts(params[:user_id], family_category_ids, @month)
     elsif params[:category_id].present?
+      @sum_target_price = Category.where(id: params[:category_id]).sum(:target_price)
       @posts = narrow_downing_posts(family_user_ids, params[:category_id], @month)
     else  # categoryもuserも指定
+      @sum_target_price = Category.where(id: params[:category_id]).sum(:target_price)
       @posts = narrow_downing_posts(params[:user_id], params[:category_id], @month)
     end
   end
+  # (1)月の変更 or (2)ユーザー絞り込み
+  # (1)例:現在9月の画面
+  # [view] posts_narrow_down_path(month: @month.prev_month) 
+  # [controller] params[:month] = @変更前month.prev_month = 2021/08
+  # [controller] @変更後month = @変更前month.prev_month = 2021/08
+  # (2)例:現在9月の画面
+  # [view] posts_narrow_down_path(month: @month)
+  # [controller] params[:month] = @変更前month = 2021/09
+  # [controller] @変更後month = @変更前month = 2021/09
   
 
   def edit
