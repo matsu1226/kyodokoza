b3d5c6112c936e564069f15b41025cd94ae6a7ba
sho matsuda <qqq.ms1126@gmail.com>
2021-12-16 13:36:13 +0900

    specの微修正/postとincomeのvalidation変更(price >= 0 => price > 0) #140

--- src/app/models/post.rb
+++ src/app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :category, foreign_key: "category_id"
 
   validates :content, presence: true, length: { maximum: 30 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :user_id, presence: true
   validates :category_id, presence: true
   validates :payment_at, presence: true
