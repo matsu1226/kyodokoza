c3bb70b23f39649235362063b57a157b59e06569
sho matsuda <qqq.ms1126@gmail.com>
2021-12-17 10:12:03 +0900

    specの修正#130

--- src/spec/system/incomes_spec.rb
+++ src/spec/system/incomes_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe "Incomes", type: :system do
       find('#hidden_field', visible: false).set('160000')
       fill_in "income_content", with: 'テスト用'
       click_button "記録"
-      expect(page).to have_selector('.alert-success', text: '記録を作成しました')
+      expect(page).to have_selector('.alert-success')
       expect(page).to have_content "収入の記録"
       # post indexのajaxが起動しないので、stat画面で確認
       click_link nil, href: stats_year_path
@@ -73,7 +73,7 @@ RSpec.describe "Incomes", type: :system do
       it "日付を編集" do
         find('#income_payment_at').set('2021-09-20')
         click_on "更新"
-        expect(page).to have_selector '.alert-post', text: '編集に成功しました'
+        expect(page).to have_selector '.alert-post'
         click_link nil, href: stats_year_path
         expect(page).to have_selector 'span', text: "¥ 260,000"
       end
@@ -81,7 +81,7 @@ RSpec.describe "Incomes", type: :system do
       it "値段を編集" do
         find('#hidden_field', visible: false).set('60000')
         click_on "更新"
-        expect(page).to have_selector '.alert-post', text: '編集に成功しました'
+        expect(page).to have_selector '.alert-post'
         click_link nil, href: stats_year_path
         expect(page).to have_selector 'span', text: "¥ 160,000"
       end
