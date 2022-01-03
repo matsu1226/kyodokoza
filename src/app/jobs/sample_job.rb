2c424452febb276637bf7cad3a7b732320273435
shotaro mastuda <qqq.ms1126@gmail.com>
2022-01-03 10:01:44 +0900

    sidekiqの導入/SampleJobのコンソール実行 #1159

--- src/app/jobs/sample_job.rb
+++ src/app/jobs/sample_job.rb
@@ -1,12 +1,7 @@
 class SampleJob < ApplicationJob
   queue_as :default
 
-  def perform(email)
-    user = User.find(email: email)
-    Post.create!(content: sidekiq_test,
-                 price: 10000,
-                 payment_at: Time.zone.now,
-                 category_id: user.relationship.categories.first.id,
-                 user_id: user.id)
+  def perform(*args)
+    puts "=====サンプルジョブです======"
   end
 end
